9890f31ed028305ed5274037aaea50fe86e3fda7
ViktorSyrotynskyi <ViktorSyrotynsky@gmail.com>
2018-10-03 14:12:48 +0300

    Cube

---
+++ 1.js
@@ -0,0 +1,5 @@
+$(function(){
+    $(document).mousemove(function(e){
+    $('#cube').css({'transform':'rotateX('+e.pageY+'deg) rotateY('+e.pageX+'deg)'});
+                          });
+})
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-!!!Hello World!!!
+cube on css
